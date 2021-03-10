0975c9fa0b9d60c83cef0fe3d800acfa81522b9a
Dexian Yang <dexianyang@Dexians-MacBook-Pro.local>
2021-03-09 17:59:41 -0800

    hi

--- race.py
+++ race.py
@@ -4,64 +4,65 @@ import time
 
 num = int(input('How Many Races Would You Like to See? '))
 
-one = turtle.Turtle()
-two = turtle.Turtle()
-ref = turtle.Turtle()
+for i in range(num):
+    one = turtle.Turtle()
+    two = turtle.Turtle()
+    ref = turtle.Turtle()
 
-one.shape("turtle")
-two.shape("turtle")
-ref.shape("turtle")
+    one.shape("turtle")
+    two.shape("turtle")
+    ref.shape("turtle")
 
-one.speed(5)
-two.speed(0)
-ref.speed(0)
+    one.speed(5)
+    two.speed(0)
+    ref.speed(0)
 
-one.color("red")
-two.color("blue")
+    one.color("red")
+    two.color("blue")
 
-one.penup()
-one.goto(-275,200)
+    one.penup()
+    one.goto(-275,200)
 
-two.penup()
-two.goto(-275, -200)
+    two.penup()
+    two.goto(-275, -200)
 
-ref.penup()
-ref.goto(-275, 400)
-ref.pendown()
-ref.right(90)
-ref.forward(800)
+    ref.penup()
+    ref.goto(-275, 400)
+    ref.pendown()
+    ref.right(90)
+    ref.forward(800)
 
-ref.penup()
-ref.goto(250, 400)
-ref.pendown()
-ref.forward(800)
+    ref.penup()
+    ref.goto(250, 400)
+    ref.pendown()
+    ref.forward(800)
 
-total_length = 525 
-distance = 0
-distance1 = 0
+    total_length = 525 
+    distance = 0
+    distance1 = 0
 
-while distance < 525 and distance1 < 525:
-    move = random.randint(0,100)
-    move1 = random.randint(0,100)
-    one.forward(move)
-    two.forward(move1)
-    time.sleep(0.5)
-    distance1 += move1
-    distance += move
-one.write("Distance traveled: " + str(distance), font=('Arial', 28 , 'italic'))
-two.write("Distance traveled: " + str(distance1),font=('Arial', 28 , 'italic'))
+    while distance < 525 and distance1 < 525:
+        move = random.randint(0,100)
+        move1 = random.randint(0,100)
+        one.forward(move)
+        two.forward(move1)
+        time.sleep(0.5)
+        distance1 += move1
+        distance += move
+    one.write("Distance traveled: " + str(distance), font=('Arial', 28 , 'italic'))
+    two.write("Distance traveled: " + str(distance1),font=('Arial', 28 , 'italic'))
 
-if distance>distance1:
-    one.right(90)
-    one.forward(20)
-    one.left(90)
-    one.write("winner yeehaw", font=('Arial', 28 , 'italic'))
-else:
-    two.right(90)
-    two.forward(20)
-    two.left(90)
-    two.write("winner yeehaw",font=('Arial', 28 , 'italic'))
+    if distance>distance1:
+        one.right(90)
+        one.forward(20)
+        one.left(90)
+        one.write("winner yeehaw", font=('Arial', 28 , 'italic'))
+    else:
+        two.right(90)
+        two.forward(20)
+        two.left(90)
+        two.write("winner yeehaw",font=('Arial', 28 , 'italic'))
 
-one.clear()
+    turtle.clearscreen()
 turtle.mainloop()
 
